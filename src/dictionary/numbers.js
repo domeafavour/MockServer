54c2ce91ea176238a19fdeab41e2aedc2d2ec9bf
zhanghaojie <zhanghaojie@midaigroup.com>
2017-09-03 11:26:06 +0800

    'remove_random_and_randomValueOfArray_instead_of_moving_them_info_random.js'

--- src/dictionary/numbers.js
+++ src/dictionary/numbers.js
@@ -1,23 +1,3 @@
-/**
- * [random description]
- * @param  {number} start start position
- * @param  {number} end   end position
- * @return {number}       random number of the range from start to end
- */
-function random(start, end) {
-  var start = start || 0
-  var end = end || 8
-  return Math.round(Math.random() * (end - start) + start )
-}
-
-/**
- * get one random value of an array
- * @param  {Array} array the array
- * @return {any}       random value of array
- */
-function randomValueOfArray (array) {
-  return array[random(0, array.length - 1)]
-}
 
 function age (start, end) {
   return random(start || 18, end || 99)
@@ -35,5 +15,6 @@ module.exports = {
   random,
   age,
   price,
-  id
+  id,
+  randomValueOfArray
 }
