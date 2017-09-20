2ee7bd91092e550d6ec8e27569259a0d72e36ada
zhanghaojie <zhanghaojie@midaigroup.com>
2017-09-20 09:13:17 +0800

    'test-del'

--- README.md
+++
@@ -1,6 +0,0 @@
-# MockServer
-mock server
-
-1. clone
-2. npm install
-3. npm run dev
--- src/dictionary/string/addresses.js
+++
@@ -1,7 +0,0 @@
-var addresses = [
-	'shanghai', 'beijing', 'shenzhen', 'guangzhou'
-]
-
-module.exports = {
-	addresses: addresses
-}
